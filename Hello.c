2bc547d1b6e052bed00c16608971ef990e989ac4
Pavel <75210735+pavel213git@users.noreply.github.com>
2021-01-20 12:24:15 +0530

    Branch changes done

--- Hello.c
+++ Hello.c
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
 	if(printf("Hello World !!! I am learning git\r\n"))
 	{}
 	printf("HI again, i have done some changes");
+	printf("Branch changes done");
 	return 0;
 }
